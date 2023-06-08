26e1747f210707248932ada5e4eed6d84b67918d
Huy P <NNewbieDev>
2023-06-08 14:47:32 +0700

    optimize genre

--- client/pages/genre/index.jsx
+++ client/pages/genre/index.jsx
@@ -1,113 +1,74 @@
 "use client";
 
-import React, { useEffect, useRef } from "react";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { allGenre, animes } from "../data";
-import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 const Genre = () => {
-  const [genres, setGenres] = useState([]);
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    focusOnSelect: true,
-    className: "center bg-black",
-    centerMode: true,
-    useCSS: true,
-    adaptiveHeight: true,
-    responsive: [
-      {
-        breakpoint: 1250,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-        },
-      },
-      {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          verticalSwiping: true,
-          swipeToSlide: true,
-          vertical: true,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          verticalSwiping: true,
-          swipeToSlide: true,
-          vertical: true,
-          arrows: false,
-        },
-      },
-    ],
+  const [type, setType] = useState("All");
+  const [indexSlice, setIndexSlice] = useState(0);
+
+  const prevSlice = () => {
+    const isValid = indexSlice < 1;
+    const prev = isValid ? Math.floor(allGenre.length / 5) : indexSlice - 1;
+    console.log(prev);
+    setIndexSlice(prev);
   };
 
-  useEffect(() => {
-    setGenres("All");
-  }, []);
+  const nextSlice = () => {
+    const isValid = indexSlice < Math.floor(allGenre.length / 5);
+    const next = isValid ? indexSlice + 1 : 0;
+    console.log(next);
+    setIndexSlice(next);
+  };
 
   return (
-    <div className="md:mx-auto md:w-4/5 w-full drop-shadow-lg box-border min-h-screen">
-      <div className="min-h-fit my-4 rounded-lg">
+    <div className="xl:mx-auto xl:w-4/5 w-full drop-shadow-lg box-border min-h-screen">
+      <div className="min-h-fit my-4 rounded-lg flex flex-col sm:flex-row justify-center gap-4 relative">
         {/* need Optimize */}
-        {/* <Slider {...settings}>
-          {allGenre.map((item, index) => (
+        {allGenre
+          .slice(indexSlice * 5, indexSlice * 5 + 5)
+          .map((item, index) => (
             <div
-              className="flex items-center justify-center cursor-pointer text-white px-4 py-3 bg-yellow-500 dark:bg-lime-400 hover:opacity-80"
+              className="flex items-center justify-center cursor-pointer rounded-md text-white px-4 py-3 bg-yellow-500 dark:bg-lime-400 hover:opacity-80"
               key={index}
-              onClick={() => setGenres(item)}
+              onClick={() => setType(item)}
             >
               <div className="text-center">{item}</div>
             </div>
           ))}
-        </Slider> */}
+        <div
+          className="absolute left-2 top-1/2 -translate-y-1/2 duration-500 text-2xl text-white dark:text-lime-200 w-12 h-12 rounded-full bg-amber-400 dark:bg-slate-500/70 cursor-pointer dark:hover:bg-slate-500 flex justify-center items-center"
+          onClick={() => prevSlice()}
+        >
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </div>
+        <div
+          className="absolute right-2 top-1/2 -translate-y-1/2 duration-500 text-2xl text-white dark:text-lime-200 w-12 h-12 rounded-full bg-amber-400 dark:bg-slate-500/70 cursor-pointer dark:hover:bg-slate-500 flex justify-center items-center"
+          onClick={() => nextSlice()}
+        >
+          <FontAwesomeIcon icon={faAngleRight} />
+        </div>
       </div>
       <div className="text-white flex items-center justify-center my-4 cursor-pointer hover:opacity-90">
         <div
           className="bg-red-500 px-6 py-2 rounded-lg"
-          onClick={() => setGenres("All")}
+          onClick={() => setType("All")}
         >
           Tất cả
         </div>
       </div>
       <div className="shadow-infull dark:shadow-lime-200 dark:bg-transparent bg-slate-100 shadow-yellow-500 rounded-lg mb-4 py-4">
-        {animes.filter((anime) => anime.genres.includes(genres)).length ===
-        0 ? (
-          <div className="text-center dark:text-lime-200">
-            Hiện chưa cập nhật anime thuộc thể loại này
-          </div>
-        ) : (
-          // cần tối ưu 2 vòng lặp
-          animes
-            .filter((anime) => anime.genres.includes(genres))
-            .map((item, index) => {
+        {animes.some((anime) => anime.genres.includes(type)) ? (
+          animes.map((item, index) => {
+            if (item.genres.includes(type)) {
               return (
                 <Link
                   href={`/anime/${item.name}`}
-                  className="inline-block w-1/2 sm:w-1/4 md:w-1/5 lg:w-1/6 text-center p-4"
+                  className="inline-block w-1/2 sm:w-1/4 md:w-1/5 xl:w-1/6 text-center p-4"
                 >
                   <div
                     className="flex flex-col items-center drop-shadow-xl sm:justify-center rounded-lg transition duration-500 ease-linear group w-full min-h-[15rem] relative"
@@ -126,15 +87,20 @@ const Genre = () => {
                     </div>
                     <div
                       className="dark:text-lime-200 group-hover:font-bold group-hover:text-red-600 dark:group-hover:text-yellow-500  group-hover:drop-shadow-xl 
-                    w-40 whitespace-nowrap overflow-hidden text-ellipsis text-center mt-2
-                    "
+                                  w-40 whitespace-nowrap overflow-hidden text-ellipsis text-center mt-2
+                                  "
                     >
                       {item.name}
                     </div>
                   </div>
                 </Link>
               );
-            })
+            }
+          })
+        ) : (
+          <div className="text-center dark:text-lime-200">
+            Hiện chưa cập nhật anime thuộc thể loại này
+          </div>
         )}
       </div>
     </div>
